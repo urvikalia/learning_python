a77b9469975246ee25c920e61076f75592a52990
urvi kalia <urvikalia@gmail.com>
2025-06-16 12:01:05 +0530

    functions

---
+++ basics/functions.py
@@ -0,0 +1,21 @@
+# Understanding functions in Python
+
+# Notes 
+# No type declaration , Python is dynamically typed
+# Identation matters , no curly braces 
+# def keyword instead of function 
+# No explicity return type declaration 
+
+
+def add(num1,num2):
+    return num1 + num2
+
+
+def subtract(num1,num2):
+    return num1 -num2 
+
+
+num1 = 12
+num2 = 5 
+print("addition is: " , add(num1,num2))
+print("Sustraction is ", subtract(num1,num2))    
